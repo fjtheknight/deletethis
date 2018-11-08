eb617c6f44bb88fff0037efb942a7f180afd50f2
fjtheknight <39682325+fjtheknight@users.noreply.github.com>
2018-11-08 00:31:38 -0500

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,2 +1,16 @@
+function listToRadioForm(l) {
+	var text = "<form>";
 
-document.getElementById("testjs").innerHTML="testing external js lol";
+    var i;
+    for (i = 0; i < free.length; i++) { 
+        text += "<input type='radio' name='time' value="+free[i]+">"+free[i]+"<br>";
+    }
+    text += "<button type='submit' onclick='replace();'>show</button></form>";
+    return text;              
+}
+function replace() {
+	document.getElementById('testjs').innerHTML='ty';        
+}
+var free = ["de a à b", "de c à d", "de e à f"];
+text=listToRadioForm(free)
+document.getElementById("testjs").innerHTML=text;
